9da479256d976e44e1ae1fdedf4eafb6423732f3
Codename O'Connell <oconnellcoder@gmail.com>
2023-05-01 21:39:47 -0400

    added a virtual for star rating average

--- server/models/Brewery.js
+++ server/models/Brewery.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 
+
 const brewerySchema = new Schema(
   {
     breweryName: {
@@ -38,11 +39,27 @@ const brewerySchema = new Schema(
   }
 );
 
+
+brewerySchema.virtual('avgRating').get(function () {
+    if(this.reviews.length){
+        let ratingSum = 0;
+        for (let i = 0; i < this.reviews.length; i++) {
+            ratingSum += this.reviews[i];
+        }
+        return ((ratingSum / reviews.length).toFixed(1));;
+    } else {
+        return null;
+    }
+});
+
+
 brewerySchema.virtual('reviewCount').get(function () {
     return this.reviews.length;
 });
 
+
 const Brewery = model("Brewery", brewerySchema);
+
 
 module.exports = Brewery;
 
