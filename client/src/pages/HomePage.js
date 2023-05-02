f70f4b887ef58215248f6d2c3156be7fae37f4bc
miacias <miaciasullo@gmail.com>
2023-05-01 20:39:23 -0400

    Added accompanying CSS to homePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -67,3 +67,14 @@ export default function HomePage() {
     </Layout>
   );
 };
+
+/*
+CSS
+
+#components-layout-demo-responsive .logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.2);
+}
+
+*/
