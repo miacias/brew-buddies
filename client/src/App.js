e48d2cd7b9b89d0ced3e9868a793e8d5a377aa25
miacias <miaciasullo@gmail.com>
2023-05-01 20:34:03 -0400

    adding responsive navbar to test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="brew-buddies">
-      <p>hi!</p>
-    </div>
+    <>
+    <Header/>
+    <HomePage/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <>
+            <h1>This will be a header component.</h1>
+        </>
+    )
+}
