277dab3400579e6f80fa1e89d1041454e4474137
miacias <miaciasullo@gmail.com>
2023-05-04 18:18:49 -0400

    added front-end authorization services

---
+++ client/src/utils/auth.js
@@ -0,0 +1,49 @@
+// decodes a token and get the user information
+import decode from 'jwt-decode';
+
+// creates a new class to instantiate for a user
+class AuthService {
+  // get user data
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  // checks if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
+
+  // checks if token is expired
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    // retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    // saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // clears user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // reloads page and resets state of application
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,24 +4,26 @@ const secret = 'cheeee33333rs';
 const expiration = '2h';
 
 module.exports = {
+  // protects routes via authentication
   authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-
+    // removes "Bearer", then returns token string
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
+    // return request if token is false
     if (!token) {
       return req;
     }
-
+    // adds the decoded user data to request to be accessed in resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
     }
-
+    // returns request object to be passed to resolver as 'context'
     return req;
   },
   signToken: function ({ email, username, _id }) {
