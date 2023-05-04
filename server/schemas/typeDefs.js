5083196235ace25e3df97e83edad03f47b7ce33f
miacias <miaciasullo@gmail.com>
2023-05-03 20:07:29 -0400

    added nodemon to serverjs packagejson

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,6 +28,11 @@ const typeDefs = gql `
         breweryId: Brewery
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
         users: [User]
         user(username: String!): User
@@ -36,7 +41,7 @@ const typeDefs = gql `
 
     type Mutation {
         addUser(username: String!, email: String!, password: String!): Auth
-        login(email: String!, password: String!): Auth
+        # login(email: String!, password: String!): Auth
         # addReview(): Auth
         # addFriend(): Auth
     }  
