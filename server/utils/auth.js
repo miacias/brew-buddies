3cf9004aa70cdcde7592893b2477b29f9c3fdd21
miaciasMAC <miaciasullo@gmail.com>
2023-05-05 19:36:20 -0400

    corrected for eslint

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,12 +5,12 @@ const expiration = '2h';
 
 module.exports = {
   // protects routes via authentication
-  authMiddleware: function ({ req }) {
+  authMiddleware({ req }) {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
     // removes "Bearer", then returns token string
     if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
+      token = token.split(' ').pop().trim();
     }
     // return request if token is false
     if (!token) {
@@ -21,12 +21,12 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log("Invalid token");
+      console.log('Invalid token');
     }
     // returns request object to be passed to resolver as 'context'
     return req;
   },
-  signToken: function ({ email, username, _id }) {
+  signToken({ email, username, _id }) {
     const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
