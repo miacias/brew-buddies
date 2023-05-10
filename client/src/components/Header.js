32e800d7d22c866bdbc8e4011522753674352f7e
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-05-10 10:03:03 -0400

    Added searchbar to navigation waiting on API to link up

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { AudioOutlined } from '@ant-design/icons';
+import { Input, Space, Row } from 'antd';
+const { Search } = Input;
 
 export default function Header() {
+    
     return (
         <>
+            <Row>
             <h1>Brew Buddies</h1>
+            <Space direction="horizontal" />
+            <Search
+            placeholder="Search by postalcode"
+            // onSearch={onSearch}
+            style={{
+                width: 200,
+            }}
+            />
+            </Row>
         </>
     )
 }
