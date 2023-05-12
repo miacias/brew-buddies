122572d8d20a6910575878cc23fa5dc43a87d916
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-05-12 18:03:03 -0400

    General cleanup of commented out trials from pages I worked on Added authorization to single brewery page // do we want to add this to just the buttons instead and have a random user be able to see a single brewery's reviews?

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Button } from 'antd' 
-import Auth from '../utils/auth'
+// import React from "react";
+// import { Button } from 'antd' 
+// import Auth from '../utils/auth'
 
-export default function LogoutButton() {
-    if (Auth.loggedIn()) {
-        return (
-            <Button>
-                <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-            </Button>
-        )
-    }
-}
+// export default function LogoutButton() {
+//     if (Auth.loggedIn()) {
+//         return (
+//             <Button>
+//                 <a href="/" onClick={() => Auth.logout()}>
+//               Logout
+//             </a>
+//             </Button>
+//         )
+//     }
+// }
