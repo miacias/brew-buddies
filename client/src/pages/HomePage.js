6534db7d0199fdec44aa612e331be18e345480e7
miacias <miaciasullo@gmail.com>
2023-05-13 10:10:11 -0400

    commented out HomePage to prevent page break

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Auth from '../utils/auth';
 import ReviewCard from '../components/ReviewCard'
 
 export default function Header() {
+    // verifies if user is logged in and sets loggedInUser State
+    const [loggedInUser, setLoggedInUser] = useState(null);
+    useEffect(() => {
+        if (Auth.loggedIn()) {
+        const userData = Auth.getProfile();
+        setLoggedInUser(userData.data);
+        }
+    }, []); // checks once
+
     return (
-        <ReviewCard/>
+        <></>
+        // <ReviewCard/>
     )
+
+
 }
