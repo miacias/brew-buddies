7b07e0b1626cc090bb96de7780f593e5213197a9
miaciasMAC <miaciasullo@gmail.com>
2023-05-14 15:15:40 -0400

    average ratings calculated client-side since Brewery model is obsolete

--- client/src/pages/SingleBrewery.js
+++ client/src/pages/SingleBrewery.js
@@ -1,19 +1,43 @@
 import { React, useState, useEffect } from "react";
 import { useMutation, useQuery } from "@apollo/client";
-// import BreweryCard from "../components/BreweryCard";
 import ReviewCard from "../components/ReviewCard";
 import AddReviewForm from '../components/AddReviewForm';
 import Auth from '../utils/auth'
 import { ADD_FAV_BREWERY } from "../utils/mutations";
 import { BREWERY_REVIEW } from '../utils/queries';
 import { useParams } from "react-router-dom";
-import { Col, Card, Button/*, Row*/ } from "antd";
+import { Col, Card, Button } from "antd";
+import { StarFilled } from '@ant-design/icons';
+
 
 export default function SingleBrewery() {
   const { breweryId } = useParams();
   const [breweryData, setBreweryData] = useState();
   const [showForm, setShowForm] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState(null);
+  const [ratings, setRatings] = useState(null);
+
+  // adds brewery to user Favorites list
+  const [addFavBrewery, { error }] = useMutation(ADD_FAV_BREWERY);
+  // loads all reviews for this brewery
+  const { loading, data } = useQuery(BREWERY_REVIEW, { variables: { breweryId }});
+
+  // calculates star review average
+  const calculateAverage = async (loading, data) => {
+    const ratings = [];
+    if (!loading && data.review) {
+      data.review.forEach(review => {
+        return ratings.push(parseInt(review.starRating));
+      });
+      const initialValue = 0;
+      const sumWithInitial = ratings.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        initialValue
+      );
+      return sumWithInitial / ratings.length;
+    }
+  }
+  calculateAverage(loading, data)
 
   // calls OpenBreweryDB API and sets breweryData State
   useEffect(() => {
@@ -32,12 +56,10 @@ export default function SingleBrewery() {
     }
   }, []); // checks once
 
-  // adds brewery to user favorites list
-  const [addFavBrewery, { error }] = useMutation(ADD_FAV_BREWERY);
-  const { loading, data } = useQuery(BREWERY_REVIEW, { variables: { breweryId }});
+
+
   // adds review to brewery page and to user profile
   // const [ addReview ] = useMutation(ADD_REVIEW);
-  
   // const _id = new ObjectId(userData.data._id);
 
   const handleAddFavBrewery = async (event) => {
@@ -54,6 +76,7 @@ export default function SingleBrewery() {
     } catch (err) {
       console.error(err);
     }
+    setRatings()
   };
 
   if(loggedInUser !== null) {
