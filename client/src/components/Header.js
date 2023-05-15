c266881d153b94d7b17271b5c75114526225326a
LearnedDr <fredrickj.chang@gmail.com>
2023-05-15 17:41:47 -0400

    fixed mobile logo issue

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import BeerLogo from './BeerLogo';
-import { Row, Col } from 'antd'
+import { Row, Col, Space } from 'antd'
 
 
 export default function Header() {
     return (
         <header>
             <Row>
+                <Space
+    direction="vertical"
+    size="middle"
+    style={{paddingBottom: '50px'}}
+  >
             <BeerLogo/>
+            </Space>
             <h1>Brew Buddies</h1>
             </Row>
         </header>
